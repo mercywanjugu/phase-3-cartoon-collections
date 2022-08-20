620dbee42fe4fc3ec1fe9a75f3331cc99d432626
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-08-20 15:15:16 +0300

    commit tree a9f638105137ff0c02b9d578f391f89ce2d1c98a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves dwarwes# code an argument here
   # Your code here
+  dwarwes.map.with_index {|dwarf,index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet planateers# code an argument here
   # Your code here
+  planateers.map{|p| p.capitalize+"!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls calls# code an argument here
   # Your code here
+  sort=calls.sort_by do |call|
+    call.length
+  
+end
+sort[-1].length>4
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find {|c| cheese_types.include?(c)}
 end
